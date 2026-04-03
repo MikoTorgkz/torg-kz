10c3de72f7297645c2d746c3c1bc5da1eadb634a
Твое имя < email>
2026-04-04 03:09:55 +0500

    clean server fix

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.json());
 
+// Главная страница
 app.get("/", (req, res) => {
   res.send("Torg KZ работает 🚀");
 });
 
+// Тест
 app.get("/test", (req, res) => {
-  res.send("OK 🚀");
+  res.send("OK ✅");
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
 });
