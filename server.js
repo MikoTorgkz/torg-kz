715f5173c67fda73fa5dabf6d9c63e05e93102cf
Твое имя < email>
2026-04-04 12:25:54 +0500

    server version 2

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-// ЛОГ (чтобы видеть запросы)
-app.use((req, res, next) => {
-  console.log("Request:", req.url);
-  next();
-});
-
-// статические файлы
-app.use(express.static(path.resolve(".")));
-
-// главная страница
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("index.html"));
+  res.status(200).send("HOME OK");
 });
 
-// fallback
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("index.html"));
+app.get("/test", (req, res) => {
+  res.status(200).send("TEST OK");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
+  console.log("SERVER VERSION 2");
   console.log("Server started on port " + PORT);
 });
