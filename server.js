fa72947e337321fb3434b36eac1e4e90042047f2
Твое имя < email>
2026-04-04 12:35:19 +0500

    fix port

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.status(200).send("HOME OK");
+  res.send("HOME OK");
 });
 
 app.get("/test", (req, res) => {
-  res.status(200).send("TEST OK");
+  res.send("TEST OK");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("SERVER VERSION 2");
+  console.log("SERVER VERSION 3");
   console.log("Server started on port " + PORT);
 });
