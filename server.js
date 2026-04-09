d3426fc95270e49bccfd58299318a1111c21c417
Твое имя < email>
2026-04-09 22:04:23 +0500

    fix db connection

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const fs = require('fs');
 const crypto = require('crypto');
 const multer = require('multer');
 const db = require('./db');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-connectionString: process.env.DATABASE_URL,
-ssl: { rejectUnauthorized: false }
-});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
